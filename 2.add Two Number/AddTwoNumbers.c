7fdfbb359ab8ebc15633d5f0c93ba18c2f28f254
anislbb <dd8271971@gmail.com>
2023-10-23 16:40:22 +0100

    adding the problme 2 and 20

---
+++ 2.add Two Number/AddTwoNumbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct ListNode
+{
+    int val;
+    struct ListNode *next;
+    
+};
+
+
+int main(int argc, char const *argv[])
+{
+    int l1[] = {2,4,3};
+    int l2[] = {5,6,4};
+
+    
+
+
+
+    return 0;
+}
